ea1e1398cb5ceb80a0acd9148e0bc1d7c0f82de2
Yakov P <yakov.petryk@gmail.com>
2022-05-18 18:11:19 +0300

    feat: add rechart library

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,14 +1,24 @@
 import { useTypedSelector } from '../../hooks/useTypedSelector'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from 'recharts'
 
 import './Graph.scss'
 
+import { getArrayForGraphRendering } from '../../helpers'
 import { userSelector } from '../../store/selectors'
 import { fetchStatisticNotifications } from '../../store/actions/user'
-
-import { useCanvas } from '../../hooks/useCanvas'
-
+import { IStatistic } from '../../types/generalTypes'
 import Loader from '../../components/Loader'
 
 export const Graph = () => {
@@ -19,35 +29,33 @@ export const Graph = () => {
     dispatch(fetchStatisticNotifications())
   }, [])
 
-  const canvasElement = useRef(null)
+  const arrayForGraphRendering: IStatistic[] = getArrayForGraphRendering(
+    notificationsForStatistic
+  )
 
-  useCanvas(canvasElement, notificationsForStatistic)
+  console.log(arrayForGraphRendering)
 
-  const canvas =
+  const char =
     loading !== 'pending' ? (
-      <div>
-        <canvas ref={canvasElement} width='800px' height='600px'></canvas>
-        <div className='widget'>
-          <ul className='description-list'>
-            <li className='description-list__item'>
-              <div className='edit-icon'></div>Edit
-            </li>
-            <li className='description-list__item'>
-              <div className='add-icon'></div>Add
-            </li>
-            <li className='description-list__item'>
-              <div className='delete-icon'></div>Delete
-            </li>
-          </ul>
-        </div>
-      </div>
+      <ResponsiveContainer width='100%' height='100%'>
+        <BarChart width={800} height={600} data={arrayForGraphRendering}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='date' />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey='add' fill='green' />
+          <Bar dataKey='edit' fill='yellow' />
+          <Bar dataKey='delete' fill='red' />
+        </BarChart>
+      </ResponsiveContainer>
     ) : null
 
   const loader = loading === 'pending' ? <Loader /> : null
 
   return (
     <div className='graph'>
-      {canvas}
+      {char}
       {loader}
     </div>
   )
